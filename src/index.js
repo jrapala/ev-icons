251d214fc1396cad5cfaa902d73b4328f6abd017
Juliette Rapala <juliette@eventric.com>
2019-11-19 16:47:03 -0600

    Create grid and add button

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-import { TweenMax } from 'gsap'
+import { TweenMax, Elastic } from 'gsap'
+
+document.querySelector('.add').addEventListener('mouseenter', addButtonEnter)
+document.querySelector('.add').addEventListener('mouseleave', addButtonLeave)
+
+function addButtonEnter() {
+	TweenMax.to('#addButton', 2, {
+		rotation: 45,
+		transformOrigin: '50% 50%',
+		ease: Elastic.easeOut.config(1, 0.3),
+	})
+	TweenMax.to('#addButton', 2, {
+		rotation: 45,
+		transformOrigin: '50% 50%',
+		ease: Elastic.easeOut.config(1, 0.3),
+	})
+}
+
+function addButtonLeave() {
+	TweenMax.to('#addButton', 2, {
+		rotation: 0,
+		transformOrigin: '50% 50%',
+		ease: Elastic.easeOut.config(1, 0.3),
+	})
+	TweenMax.to('#addButton', 2, {
+		rotation: 0,
+		transformOrigin: '50% 50%',
+		ease: Elastic.easeOut.config(1, 0.3),
+	})
+}
